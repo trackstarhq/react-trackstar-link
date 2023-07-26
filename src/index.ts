8ca1675b2c3955e129773b0febe99cb36ccf18db
Harsha Nandiwada <harsha@trackstarhq.com>
2023-07-26 13:25:54 -0400

    working export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import TrackstarConnectButton from "./TrackstarConnectButton";
 import useTrackstarLink from "./useTrackstarLink";
 
-console.log('TrackstarConnectButton', TrackstarConnectButton);
-console.log('useTrackstarLink', useTrackstarLink);
-export default { TrackstarConnectButton, useTrackstarLink };
+export { TrackstarConnectButton };
+export {  useTrackstarLink };

8dc8db21dffc2f88955941c3fa885c6e5247a55f
ryanjpaul <102236791+ryanjpaul@users.noreply.github.com>
2024-01-10 15:14:03 -0500

    integrationswithendpoints support + readme (tra-516) (#4)
    * integrationswithendpoints support + readme
    
    * enum allowed endpoints
    
    * add get_bills to enum

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,7 @@
+type AllowedEndpoints = "get_inventory" | "get_products" | "create_product" | "update_product" | "get_inbound_shipment" | "create_inbound_shipment"
+  | "update_inbound_shipment" | "get_orders" | "create_order" | "update_order" | "get_returns" | "create_return" | "update_return"
+  | "get_shipping_methods" | "get_bills" | "get_warehouses" | "get_warehouse_customers" | "get_labor_activities";
+
 export interface ClientConfig {
   onSuccess: (authCode: string) => void;
   onClose?: () => void;
@@ -5,6 +9,7 @@ export interface ClientConfig {
   getLinkToken: () => Promise<string>;
   integrationAllowList?: string[];
   integrationBlockList?: string[];
+  integrationsWithEndpoints: AllowedEndpoints[];
   buttonId?: string;
   sandbox?: boolean;
 }

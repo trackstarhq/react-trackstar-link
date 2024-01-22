7c621d70eb709952a14f055e2d5bf8a134cfe5ed
Harsha Nandiwada <harsha@trackstarhq.com>
2024-01-22 13:16:11 -0800

    updated AllowedEndpoints

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 type AllowedEndpoints = "get_inventory" | "get_products" | "create_product" | "update_product" | "get_inbound_shipment" | "create_inbound_shipment"
-  | "update_inbound_shipment" | "get_orders" | "create_order" | "update_order" | "get_returns" | "create_return" | "update_return"
+  | "update_inbound_shipment" | "get_orders" | "create_order" | "update_order" | "create_kit" | "update_kit" | "get_returns" | "create_return" | "update_return"
   | "get_shipping_methods" | "get_bills" | "get_warehouses" | "get_warehouse_customers" | "get_labor_activities";
 
 export interface ClientConfig {
